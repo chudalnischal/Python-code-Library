2ebd16ea21b1a6b4d755a186a8ded31a22198900
Nischal CHudal <chudalnischal7@gmail.com>
2024-05-28 15:12:32 -0400

    for jenkins operation

---
+++ main.py
@@ -0,0 +1,14 @@
+import datetime
+
+present_date = datetime.datetime.now()
+print(present_date)
+
+for i in range(1,21):
+    if i == 10:
+        print("it is a solid number")
+    elif i == 15:
+        print ("it is slightly bigger")
+    elif i == 20:
+        print("solid hit number")
+          
+
